0b208c084f279226b0f681e37fb338b6a0eaa961
dodiman <dodimantakimpoo@gmail.com>
2021-06-03 08:33:43 +0800

    3 tambahkan paragraf baru di html

--- webku/settings.py
+++ webku/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-qx_wmx-l1-jldb@u9@2(%#kw)88u0p3fyt1spv!3#-620zmit=
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'webku.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': ['templates'],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
